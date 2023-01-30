b7974d1bcf4c74c4d35fedc1e171a13ffdb7473e
Rafael Daminelli <rdaminelli@hotmail.com>
2023-01-29 22:45:03 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import {Route, Routes} from "react-router-dom"
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import DetailScreen from "./components/detailComponents/DetailScreen"
+import HomeScreen from "./components/homeComponents/HomeScreen";
+import NewRecipeScreen from  "./components/newRecipeComponents/NewRecipeScreen"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route index element={<HomeScreen />} />
+        <Route path="newRecipe" element={<NewRecipeScreen />} />
+        <Route path="recipe/:id" element={<DetailScreen />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
